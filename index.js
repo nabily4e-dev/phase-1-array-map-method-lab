6c66c64372325e43793f2291e862ff7d23a90b3d
nabily4e-dev <nbl.dev21@gmail.com>
2023-01-16 17:01:35 +0300

    Completed assignment!

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+// بسم الله
+
+const tutorials = [
+	"what does the this keyword mean?",
+	"What is the Constructor OO pattern?",
+	"implementing Blockchain Web API",
+	"The Test Driven Development Workflow",
+	"What is NaN and how Can we Check for it",
+	"What is the difference between stopPropagation and preventDefault?",
+	"Immutable State and Pure Functions",
+	"what is the difference between == and ===?",
+	"what is the difference between event capturing and bubbling?",
+	"what is JSONP?",
+];
+
 function titleCased() {
-  return [
-    "What Does The This Keyword Mean?",
-    "What Is The Constructor OO Pattern?",
-    "Implementing Blockchain Web API",
-    "The Test Driven Development Workflow",
-    "What Is NaN And How Can We Check For It",
-    "What Is The Difference Between StopPropagation And PreventDefault?",
-    "Immutable State And Pure Functions",
-    "What Is The Difference Between == And ===?",
-    "What Is The Difference Between Event Capturing And Bubbling?",
-    "What Is JSONP?",
-  ];
+	return tutorials.map((tutorial) => {
+		let words = tutorial.split(" ");
+		words = words.map(word => word[0].toUpperCase() + word.substring(1));
+		return words.join(" ");
+	});
 }
+
+console.log(titleCased());
+
+// const robots = [
+// 	{ name: "Johnny 5", modes: 5, isActivated: false },
+// 	{ name: "C3PO", modes: 3, isActivated: false },
+// 	{ name: "Sonny", modes: 2.5, isActivated: false },
+// 	{ name: "Baymax", modes: 1.5, isActivated: false },
+// ];
+
+// const activatedRobots = robots.map((robot) => {
+// 	return Object.assign({}, robot, {
+// 		modes: robot.modes * 2,
+// 		isActivated: true,
+// 	});
+// });
+
+// console.log(activatedRobots);
